25cdec6f76862fbfc76719893d0971ba546e3814
Thandar Lin <135203106+thandarlin19@users.noreply.github.com>
2023-12-01 10:40:52 +0630

    Update App.js
    API Call Testing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  const myFunction = async () => {
+    try {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+      const data = await response.json();
+      console.log('API Data:', data);
+    } catch (error) {
+      console.error('Error calling API:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World
-        </p>
+        <p>Hello World</p>
+        <button onClick={myFunction}>Go to API</button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+
